3699bc3b050e6fcb8f309c94508db0408d63bf68
Tony Ruiz <tonyruizo@pm.me>
2025-05-17 07:35:30 -0400

    Add search functionality

--- InventoryManagementSystem/Forms/AddProductForm.Designer.cs
+++ InventoryManagementSystem/Forms/AddProductForm.Designer.cs
@@ -28,13 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             this.cancelButton = new System.Windows.Forms.Button();
             this.pageTitleLabel = new System.Windows.Forms.Label();
+            this.productMaxTextBox = new System.Windows.Forms.TextBox();
+            this.productMaxLabel = new System.Windows.Forms.Label();
+            this.productMinTextBox = new System.Windows.Forms.TextBox();
+            this.productMinLabel = new System.Windows.Forms.Label();
+            this.productPriceTextBox = new System.Windows.Forms.TextBox();
+            this.productPriceLabel = new System.Windows.Forms.Label();
+            this.productInventoryTextBox = new System.Windows.Forms.TextBox();
+            this.productInventoryLabel = new System.Windows.Forms.Label();
+            this.productNameTextBox = new System.Windows.Forms.TextBox();
+            this.productNameLabel = new System.Windows.Forms.Label();
+            this.productDTextBox = new System.Windows.Forms.TextBox();
+            this.productIDLabel = new System.Windows.Forms.Label();
+            this.partSearchText = new System.Windows.Forms.TextBox();
+            this.PartSearchButton = new System.Windows.Forms.Button();
+            this.dgvParts = new System.Windows.Forms.DataGridView();
+            this.dgvProducts = new System.Windows.Forms.DataGridView();
+            this.candidatePartsLabel = new System.Windows.Forms.Label();
+            this.associatedProductLabel = new System.Windows.Forms.Label();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvParts)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).BeginInit();
             this.SuspendLayout();
             // 
             // cancelButton
             // 
-            this.cancelButton.Location = new System.Drawing.Point(1022, 809);
+            this.cancelButton.Location = new System.Drawing.Point(1684, 946);
             this.cancelButton.Name = "cancelButton";
             this.cancelButton.Size = new System.Drawing.Size(123, 55);
             this.cancelButton.TabIndex = 13;
@@ -52,15 +79,251 @@
             this.pageTitleLabel.TabIndex = 12;
             this.pageTitleLabel.Text = "Page Title";
             // 
+            // productMaxTextBox
+            // 
+            this.productMaxTextBox.Location = new System.Drawing.Point(413, 641);
+            this.productMaxTextBox.Name = "productMaxTextBox";
+            this.productMaxTextBox.Size = new System.Drawing.Size(85, 31);
+            this.productMaxTextBox.TabIndex = 33;
+            // 
+            // productMaxLabel
+            // 
+            this.productMaxLabel.AutoSize = true;
+            this.productMaxLabel.Location = new System.Drawing.Point(304, 641);
+            this.productMaxLabel.Name = "productMaxLabel";
+            this.productMaxLabel.Size = new System.Drawing.Size(53, 25);
+            this.productMaxLabel.TabIndex = 37;
+            this.productMaxLabel.Text = "Max";
+            // 
+            // productMinTextBox
+            // 
+            this.productMinTextBox.Location = new System.Drawing.Point(184, 644);
+            this.productMinTextBox.Name = "productMinTextBox";
+            this.productMinTextBox.Size = new System.Drawing.Size(85, 31);
+            this.productMinTextBox.TabIndex = 32;
+            // 
+            // productMinLabel
+            // 
+            this.productMinLabel.AutoSize = true;
+            this.productMinLabel.Location = new System.Drawing.Point(35, 647);
+            this.productMinLabel.Name = "productMinLabel";
+            this.productMinLabel.Size = new System.Drawing.Size(47, 25);
+            this.productMinLabel.TabIndex = 36;
+            this.productMinLabel.Text = "Min";
+            // 
+            // productPriceTextBox
+            // 
+            this.productPriceTextBox.Location = new System.Drawing.Point(184, 556);
+            this.productPriceTextBox.Name = "productPriceTextBox";
+            this.productPriceTextBox.Size = new System.Drawing.Size(252, 31);
+            this.productPriceTextBox.TabIndex = 30;
+            // 
+            // productPriceLabel
+            // 
+            this.productPriceLabel.AutoSize = true;
+            this.productPriceLabel.Location = new System.Drawing.Point(35, 559);
+            this.productPriceLabel.Name = "productPriceLabel";
+            this.productPriceLabel.Size = new System.Drawing.Size(61, 25);
+            this.productPriceLabel.TabIndex = 34;
+            this.productPriceLabel.Text = "Price";
+            // 
+            // productInventoryTextBox
+            // 
+            this.productInventoryTextBox.Location = new System.Drawing.Point(184, 475);
+            this.productInventoryTextBox.Name = "productInventoryTextBox";
+            this.productInventoryTextBox.Size = new System.Drawing.Size(252, 31);
+            this.productInventoryTextBox.TabIndex = 29;
+            // 
+            // productInventoryLabel
+            // 
+            this.productInventoryLabel.AutoSize = true;
+            this.productInventoryLabel.Location = new System.Drawing.Point(35, 478);
+            this.productInventoryLabel.Name = "productInventoryLabel";
+            this.productInventoryLabel.Size = new System.Drawing.Size(100, 25);
+            this.productInventoryLabel.TabIndex = 31;
+            this.productInventoryLabel.Text = "Inventory";
+            // 
+            // productNameTextBox
+            // 
+            this.productNameTextBox.Location = new System.Drawing.Point(184, 401);
+            this.productNameTextBox.Name = "productNameTextBox";
+            this.productNameTextBox.Size = new System.Drawing.Size(252, 31);
+            this.productNameTextBox.TabIndex = 27;
+            // 
+            // productNameLabel
+            // 
+            this.productNameLabel.AutoSize = true;
+            this.productNameLabel.Location = new System.Drawing.Point(35, 404);
+            this.productNameLabel.Name = "productNameLabel";
+            this.productNameLabel.Size = new System.Drawing.Size(68, 25);
+            this.productNameLabel.TabIndex = 28;
+            this.productNameLabel.Text = "Name";
+            // 
+            // productDTextBox
+            // 
+            this.productDTextBox.Location = new System.Drawing.Point(184, 321);
+            this.productDTextBox.Name = "productDTextBox";
+            this.productDTextBox.ReadOnly = true;
+            this.productDTextBox.Size = new System.Drawing.Size(252, 31);
+            this.productDTextBox.TabIndex = 26;
+            // 
+            // productIDLabel
+            // 
+            this.productIDLabel.AutoSize = true;
+            this.productIDLabel.Location = new System.Drawing.Point(35, 321);
+            this.productIDLabel.Name = "productIDLabel";
+            this.productIDLabel.Size = new System.Drawing.Size(32, 25);
+            this.productIDLabel.TabIndex = 25;
+            this.productIDLabel.Text = "ID";
+            // 
+            // partSearchText
+            // 
+            this.partSearchText.Location = new System.Drawing.Point(1410, 64);
+            this.partSearchText.Name = "partSearchText";
+            this.partSearchText.Size = new System.Drawing.Size(378, 31);
+            this.partSearchText.TabIndex = 38;
+            // 
+            // PartSearchButton
+            // 
+            this.PartSearchButton.Location = new System.Drawing.Point(1282, 61);
+            this.PartSearchButton.Name = "PartSearchButton";
+            this.PartSearchButton.Size = new System.Drawing.Size(122, 37);
+            this.PartSearchButton.TabIndex = 39;
+            this.PartSearchButton.Text = "Search";
+            this.PartSearchButton.UseVisualStyleBackColor = true;
+            this.PartSearchButton.Click += new System.EventHandler(this.PartSearchButton_Click);
+            // 
+            // dgvParts
+            // 
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvParts.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvParts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvParts.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvParts.Location = new System.Drawing.Point(610, 117);
+            this.dgvParts.Name = "dgvParts";
+            this.dgvParts.RowHeadersWidth = 82;
+            this.dgvParts.RowTemplate.Height = 33;
+            this.dgvParts.Size = new System.Drawing.Size(1197, 319);
+            this.dgvParts.TabIndex = 40;
+            this.dgvParts.TabStop = false;
+            // 
+            // dgvProducts
+            // 
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvProducts.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvProducts.DefaultCellStyle = dataGridViewCellStyle4;
+            this.dgvProducts.Location = new System.Drawing.Point(610, 563);
+            this.dgvProducts.Name = "dgvProducts";
+            this.dgvProducts.RowHeadersWidth = 82;
+            this.dgvProducts.RowTemplate.Height = 33;
+            this.dgvProducts.Size = new System.Drawing.Size(1197, 319);
+            this.dgvProducts.TabIndex = 41;
+            this.dgvProducts.TabStop = false;
+            // 
+            // candidatePartsLabel
+            // 
+            this.candidatePartsLabel.AutoSize = true;
+            this.candidatePartsLabel.Location = new System.Drawing.Point(605, 67);
+            this.candidatePartsLabel.Name = "candidatePartsLabel";
+            this.candidatePartsLabel.Size = new System.Drawing.Size(192, 25);
+            this.candidatePartsLabel.TabIndex = 42;
+            this.candidatePartsLabel.Text = "All candidate Parts";
+            // 
+            // associatedProductLabel
+            // 
+            this.associatedProductLabel.AutoSize = true;
+            this.associatedProductLabel.Location = new System.Drawing.Point(610, 518);
+            this.associatedProductLabel.Name = "associatedProductLabel";
+            this.associatedProductLabel.Size = new System.Drawing.Size(338, 25);
+            this.associatedProductLabel.TabIndex = 43;
+            this.associatedProductLabel.Text = "Parts Associated with this Product";
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(1678, 453);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(129, 50);
+            this.AddButton.TabIndex = 44;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = true;
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Location = new System.Drawing.Point(1532, 951);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(129, 50);
+            this.DeleteButton.TabIndex = 45;
+            this.DeleteButton.Text = "Delete";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(1379, 951);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(129, 50);
+            this.SaveButton.TabIndex = 46;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            // 
             // AddProductForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1217, 933);
+            this.ClientSize = new System.Drawing.Size(1898, 1074);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.associatedProductLabel);
+            this.Controls.Add(this.candidatePartsLabel);
+            this.Controls.Add(this.dgvProducts);
+            this.Controls.Add(this.dgvParts);
+            this.Controls.Add(this.partSearchText);
+            this.Controls.Add(this.PartSearchButton);
+            this.Controls.Add(this.productMaxTextBox);
+            this.Controls.Add(this.productMaxLabel);
+            this.Controls.Add(this.productMinTextBox);
+            this.Controls.Add(this.productMinLabel);
+            this.Controls.Add(this.productPriceTextBox);
+            this.Controls.Add(this.productPriceLabel);
+            this.Controls.Add(this.productInventoryTextBox);
+            this.Controls.Add(this.productInventoryLabel);
+            this.Controls.Add(this.productNameTextBox);
+            this.Controls.Add(this.productNameLabel);
+            this.Controls.Add(this.productDTextBox);
+            this.Controls.Add(this.productIDLabel);
             this.Controls.Add(this.cancelButton);
             this.Controls.Add(this.pageTitleLabel);
             this.Name = "AddProductForm";
             this.Text = "Product";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvParts)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -70,5 +333,26 @@
 
         private System.Windows.Forms.Button cancelButton;
         private System.Windows.Forms.Label pageTitleLabel;
+        private System.Windows.Forms.TextBox productMaxTextBox;
+        private System.Windows.Forms.Label productMaxLabel;
+        private System.Windows.Forms.TextBox productMinTextBox;
+        private System.Windows.Forms.Label productMinLabel;
+        private System.Windows.Forms.TextBox productPriceTextBox;
+        private System.Windows.Forms.Label productPriceLabel;
+        private System.Windows.Forms.TextBox productInventoryTextBox;
+        private System.Windows.Forms.Label productInventoryLabel;
+        private System.Windows.Forms.TextBox productNameTextBox;
+        private System.Windows.Forms.Label productNameLabel;
+        private System.Windows.Forms.TextBox productDTextBox;
+        private System.Windows.Forms.Label productIDLabel;
+        private System.Windows.Forms.TextBox partSearchText;
+        private System.Windows.Forms.Button PartSearchButton;
+        private System.Windows.Forms.DataGridView dgvParts;
+        private System.Windows.Forms.DataGridView dgvProducts;
+        private System.Windows.Forms.Label candidatePartsLabel;
+        private System.Windows.Forms.Label associatedProductLabel;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Button DeleteButton;
+        private System.Windows.Forms.Button SaveButton;
     }
 }
--- InventoryManagementSystem/Forms/AddProductForm.cs
+++ InventoryManagementSystem/Forms/AddProductForm.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Forms;
+﻿using InventoryManagementSystem.Models;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows.Forms;
 
 namespace InventoryManagementSystem.Forms
 {
@@ -8,8 +11,51 @@ namespace InventoryManagementSystem.Forms
         {
             InitializeComponent();
             pageTitleLabel.Text = "Add Product";
+            dgvParts.DataSource = Inventory.AllParts;
+            dgvProducts.DataSource = Inventory.Products;
         }
 
+        private void PartSearchButton_Click(object sender, System.EventArgs e)
+        {
+            string searchedPart = partSearchText.Text.Trim().ToLower();
+            bool isPart = false;
+
+            // Show all parts if field is empty
+            if (string.IsNullOrWhiteSpace(searchedPart))
+            {
+                dgvParts.DataSource = null;
+                dgvParts.DataSource = Inventory.AllParts;
+                return;
+            }
+
+            // Search by ID
+            if (int.TryParse(searchedPart, out int partId))
+            {
+                var match = Inventory.AllParts.FirstOrDefault(p => p.PartID == partId);
+                if (match != null)
+                {
+                    dgvParts.DataSource = new BindingList<Part> { match };
+                    isPart = true;
+                }
+            }
+            else
+            {
+                // Search by Name
+                var match = Inventory.AllParts.Where(p => p.Name.ToLower().Contains(searchedPart)).ToList();
+                if (match.Any())
+                {
+                    dgvParts.DataSource = new BindingList<Part>(match);
+                    isPart = true;
+                }
+            }
+
+            if (!isPart)
+            {
+                MessageBox.Show("Part not found.");
+                dgvParts.DataSource = null;
+                dgvParts.DataSource = Inventory.AllParts;
+            }
+        }
 
 
         // Cancel
@@ -17,5 +63,7 @@ namespace InventoryManagementSystem.Forms
         {
             this.Close();
         }
+
+
     }
 }
